f0660a8497209fba4fe509ba2df83b969e5be065
KhanzaidGithub <zaidscestudent@gmail.com>
2024-07-17 00:45:36 -0700

    Not completed

---
+++ app.js
@@ -0,0 +1,11 @@
+// APis links
+// https://free-apis.github.io/#/browse
+// https://alexwohlbruck.github.io/cat-facts/docs/
+// https://github.com/fawazahmed0/currency-api#readme
+
+
+const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+for (let code in countryList) {
+    console.log(code);
+}
---
+++ script.js
@@ -0,0 +1,36 @@
+
+// let url = 'https://cat-fact.herokuapp.com/facts'; // Api Link
+// let factpara = document.getElementById('para');
+// let btn = document.getElementById('btn');
+// // let promise = fetch(url);
+// // console.log(promise);   // This has returned a succesfull data
+
+// // we have many wasy to get data such as Promise chaining , Callback Hell but we will use Async & Await to get data
+
+
+
+// // let getfacts = async () => {
+// //     console.log('gettting data');
+// //     let respond = await fetch(url);
+// //     console.log(respond); // JSON Formate Javascript Object Notation
+// //     let data = await respond.json();
+// //     console.log(data[0+1].text);
+// //     // factpara.innerHTML = data[0].text; // To display on HTML dom
+// // };
+// // btn.addEventListener('click', getfacts);
+
+
+// function getfacts() {
+//     fetch(url).then((respond) => {
+//         return respond.json();
+//     }).then((data) => {
+//         console.log(data);
+//         factpara.innerHTML = data[0].text;
+//     });// This is called promise chaining we have done 
+// }
+// btn.addEventListener('click', getfacts);
+
+// // (Input is JSON, outout is Javascript Object)
+
+// // before modern javascript we used to get apis ata in XML formate but in modern javascript we get data in JSOn Formate
+
