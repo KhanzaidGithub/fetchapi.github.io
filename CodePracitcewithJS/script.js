017065174a5b27b3b9cbc3ea6a5112cb034ba463
KhanzaidGithub <zaidscestudent@gmail.com>
2024-09-09 11:47:10 -0700

    Practice OF animation

---
+++ CodePracitcewithJS/script.js
@@ -0,0 +1,23 @@
+let elemcon = document.getElementById('elem-container');
+let fixed = document.getElementById('fixed-img')
+
+elemcon.addEventListener('mouseenter', () => {
+    fixed.style.display = 'block'
+})
+elemcon.addEventListener('mouseleave', () => {
+    fixed.style.display = 'none';
+})
+
+// let elems = document.getElementById('elem1');
+// elems.addEventListener('mouseenter', () => {
+//     let image = elems.getAttribute('data-image');
+//     fixed.style.backgroundImage = `url(${image})`
+// })
+
+let elems = document.querySelectorAll('.elem');
+elems.forEach((e) => {
+    e.addEventListener('mouseenter', () => {
+        let image = e.getAttribute('data-image');
+        fixed.style.backgroundImage = `url(${image})`
+    })
+})
